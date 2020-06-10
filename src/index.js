282927a237c8149fbb42e2ab2f76049d07e4524c
asosunoff <al.sosunoff@gmail.com>
2020-06-10 21:28:37 +0300

    added middleware

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './redux/rootReducer';
 
-const store = createStore(rootReducer);
+const loggerMiddleware = store => next => action => {
+	const result = next(action);
+	console.log('Middleware', store.getState());
+	return result;
+};
+
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 // <React.StrictMode>
 // </React.StrictMode>
 const app = (
